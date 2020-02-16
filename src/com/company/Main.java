1965078341f5b46b1ead1e6bf7d6d06fc253ebea
dawid <dawidgorski92@gmail.com>
2020-02-16 01:30:46 +0100

    dodana obsługa z konsoli,

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,59 +1,63 @@
 package com.company;
 
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.sql.Date;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 
 public class Main {
 
-    public static void RetreiveRecords(int r){
-
-
-
-
-    }
-    public static void main(String[] args) throws SQLException, IOException {
-	        TableService ts1=new TableService();
-       // System.out.println(ts1.recordRetreive(22));
-      //  System.out.println(ts1.recordInsert("JOHN","DEEP"));
-       // System.out.println(ts1.recordUpdate(56,"Jan","Kowalski"));
-        System.out.println(ts1.recordDelete("JOHN","DEEP"));
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*    Connection conn;
+    public static void main(String[] args) throws IOException, SQLException {
+        System.out.println("Select one action: CREATE, READ, UPDATE, DELETE, LIST TABLE");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        TableService ts1 = new TableService();
+        String action = "";
         try {
-            DriverManager.registerDriver(new org.postgresql.Driver());
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
-            // ...
-
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM author");
-
-        while(rs.next()) {
-            System.out.println(rs.getInt("id") + " : " + rs.getString("name"));
-        }
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch ( SQLException e) {
+            action = br.readLine();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-*/
-
-
+        switch (action) {
+            case "CREATE": {
+                System.out.println("enter first name");
+                String firstName = br.readLine();
+                System.out.println("enter last name");
+                String lastName = br.readLine();
+                System.out.println("Dodano:\n " + ts1.recordInsert(firstName, lastName));
+            }
+            break;
+            case "READ": {
+                System.out.println("enter id number");
+                int id = Integer.parseInt(br.readLine());
+                System.out.println(ts1.recordRead(id));
+            }
+            break;
+            case "LIST TABLE": {
+                System.out.println(ts1.recordRead());
+            }
+            break;
+            case "UPDATE": {
+                System.out.println("enter id number");
+                int id = Integer.parseInt(br.readLine());
+                System.out.println("enter new first name");
+                String firstName = br.readLine();
+                System.out.println("enter new last name");
+                String lastName = br.readLine();
+                System.out.println(ts1.recordUpdate(id, firstName, lastName));
+            }
+            break;
+            case "DELETE": {
+                System.out.println("enter id number");
+                int id = Integer.parseInt(br.readLine());
+                System.out.println("enter first name");
+                String firstName = br.readLine();
+                System.out.println("enter last name");
+                String lastName = br.readLine();
+                System.out.println(ts1.recordDelete(id, firstName, lastName));
+            }
+            break;
+            default:
+                System.out.println("Wrong input");
+        }
+    }
 }
--- src/com/company/TableService.java
+++ src/com/company/TableService.java
@@ -1,10 +1,8 @@
 package com.company;
 
-
 import java.io.*;
 import java.sql.*;
 import java.text.SimpleDateFormat;
-
 
 public class TableService {
     private int actor_id;
@@ -12,96 +10,89 @@ public class TableService {
     private String last_name;
     private String last_update = "2020-01-06";
 
-
     Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila?user=root&password=Noobplx0&serverTimezone=UTC");
     Statement stmt = conn.createStatement();
     ResultSet rs = null;
     String result = "";
     SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-    java.sql.Date currentDate =new java.sql.Date(new java.util.Date().getTime());
-    Writer output = new BufferedWriter(new FileWriter("uzytkownicy.txt", true));
+    java.sql.Date currentDate = new java.sql.Date(new java.util.Date().getTime());
+    Writer output = new BufferedWriter(new FileWriter("DataLog.txt", true));
 
-    public TableService() throws SQLException, IOException {
-    }
+    public TableService() throws SQLException, IOException { }
 
-    public String recordRetreive(int r) throws SQLException {
-
-
+    public String recordRead(int r) throws SQLException {
         try {
-
             rs = stmt.executeQuery("SELECT *FROM actor where actor_id=" + r + "");
-            while (rs.next()) {
-                actor_id = rs.getInt(1);
-                first_name = rs.getString(2);
-                last_name = rs.getString(3);
-                last_update = rs.getString(4);
-                result += actor_id + " " + first_name + " " + last_name + " " + last_update;
-            }
+            recordReadResultAppend();
             conn.close();
-
-
-            output.append("SELECT: "+result + "\n");
+            output.append("SELECT : " + result + "\n");
             output.close();
-
         } catch (SQLException e) {
-            System.out.println("błąd w uzyskiwaniu danych z bazy MySQL:" + e);
-
+            System.out.println("Data selecting error:" + e);
         } catch (IOException e) {
-            System.out.println("bła przy zapisie danych do pliku: " + e);
-
+            System.out.println(".: " + e);
         }
-
-
         return result;
     }
-
+    public String recordRead() throws SQLException {
+        try {
+            rs = stmt.executeQuery("SELECT *FROM actor ");
+            recordReadResultAppend();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Data selecting error:" + e);
+        }
+        return result;
+    }
+    public void recordReadResultAppend() throws SQLException {
+        while (rs.next()) {
+            actor_id = rs.getInt(1);
+            first_name = rs.getString(2);
+            last_name = rs.getString(3);
+            last_update = rs.getString(4);
+            result += first_name + " " + last_name + " " + last_update;
+        }
+    }
 
     public String recordInsert(String first_name, String last_name) {
         try {
-
             stmt = conn.createStatement();
-            stmt.executeUpdate("INSERT INTO actor VALUES(NULL,'" + first_name + "','" + last_name + "',"+"'"+sdf3.format(currentDate)+"'"+")");
+            stmt.executeUpdate("INSERT INTO actor VALUES(NULL,'" + first_name + "','" + last_name + "'," + "'" + sdf3.format(currentDate) + "'" + ")");
             result = first_name + " " + last_name + " " + sdf3.format(currentDate);
-
             conn.close();
-            output.append("INSERT : "+result + "\n");
+            output.append("INSERT : " + result + "\n");
             output.close();
         } catch (SQLException | IOException e) {
-            System.out.println("błąd we wprowadzaniu danych do bazy MySQL:" + e);
+            System.out.println("Data inserting error: " + e);
         }
         return result;
     }
 
-    public String recordDelete(String first_name, String last_name) {
-
+    public String recordDelete(int actor_id, String first_name, String last_name) {
         try {
             stmt = conn.createStatement();
-            stmt.executeUpdate("DELETE FROM sakila.actor WHERE first_name=" + "'" + first_name + "'" + "AND " + "last_name=" + "'" + last_name + "'");
+            stmt.executeUpdate("DELETE FROM sakila.actor WHERE actor_id=" + "'" + actor_id + "'" + " AND " + " first_name=" + "'" + first_name + "'" + " AND " + "last_name=" + "'" + last_name + "'");
             result = first_name + " " + last_name + " " + sdf3.format(currentDate);
-            output.append("DELETE : "+result + "\n");
+            output.append("DELETE : " + result + "\n");
             output.close();
             conn.close();
         } catch (SQLException | IOException e) {
-            System.out.println("błąd we wprowadzaniu danych do bazy MySQL:" + e);
-
+            System.out.println("Data deleting error" + e);
         }
-
         return result;
     }
+
     public String recordUpdate(Integer actor_id, String first_name, String last_name) {
-
         try {
             stmt = conn.createStatement();
-            stmt.executeUpdate("UPDATE sakila.actor SET first_name=" + "'" + first_name + "'" + "," + "last_name=" + "'" + last_name + "'"+","+"last_update="+"'"+sdf3.format(currentDate)+"'"+" WHERE "+"actor_id="+"'"+actor_id+"'");//???
+            stmt.executeUpdate("UPDATE sakila.actor SET first_name=" + "'" + first_name + "'" + "," + "last_name=" + "'" + last_name + "'" + "," + "last_update=" + "'" + sdf3.format(currentDate) + "'" + " WHERE " + "actor_id=" + "'" + actor_id + "'");//???
             result = first_name + " " + last_name + " " + sdf3.format(currentDate);
-            output.append("UPDATE : "+result + "\n");
+            output.append("UPDATE : " + result + "\n");
             output.close();
             conn.close();
         } catch (SQLException | IOException e) {
-            System.out.println("błąd we wprowadzaniu danych do bazy MySQL:" + e);
-
+            System.out.println("Updating error" + e);
         }
-
-    return result;
+        return result;
     }
 }
